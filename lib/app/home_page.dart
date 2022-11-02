7f1b9fdd9714a2accacc30b1cf2b2c4896f33f19
Marcelo Figueira <marcelofigueira87@gmail.com>
2022-11-01 22:54:02 -0300

    create email_sotre and mobx config

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,15 +1,50 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:test_mobx/stores/email_store.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    final emailStore = EmailStore();
+
+    void _nextStep() {
+      print("botao ativado");
+    }
+
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                onChanged: emailStore.changeEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "E-mail", labelStyle: TextStyle(fontSize: 30.0)),
+              ),
+              Observer(
+                builder: (_) => MaterialButton(
+                  minWidth: double.infinity,
+                  onPressed: emailStore.buttonEnabled ? _nextStep : null,
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  disabledColor: Colors.grey,
+                  child: const Text(
+                    'Continuar',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
